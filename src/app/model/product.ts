92cefe9c3bb24a9f5bb9bb4fb96b49c7cfac5fd5
Anankaaa <hegyaljai83@hotmail.com>
2022-11-10 21:19:19 +0100

    adding productService

---
+++ src/app/model/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+  id: number = 0;
+  categoryId: number = 0;
+  name: string = '';
+  description: string = '';
+  img_src: string = '';
+  price: number = 0;
+  stock: number = 0;
+  featured: boolean = false;
+  active: boolean = false;
+}
