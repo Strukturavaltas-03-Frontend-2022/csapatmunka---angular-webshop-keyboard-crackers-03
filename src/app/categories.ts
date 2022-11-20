434ee07624f60be45f0a8ef7dbf91e66d578a984
utnesz <90829509+utnesz@users.noreply.github.com>
2022-11-20 17:08:53 +0100

    Background a bit darker

---
+++ src/app/categories.ts
@@ -0,0 +1,9 @@
+import { Category } from './model/category';
+
+export const category: Category[] =
+ [
+  {"id":0,"name":"Painkillers","description":"Medications for all your pains..."}
+ ,{"id":1,"name":"Sedatives","description":"In case you need a break..."}
+ ,{"id":2,"name":"Stimulants","description":"If you need some boost..."}
+ ,{"id":3,"name":"Illegal stuffs","description":"DON'T TELL ANYONE"}
+ ]
